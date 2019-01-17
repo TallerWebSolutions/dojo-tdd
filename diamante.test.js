6250704addda6d39643709827fc4b8d2e35900ba
Sebastian Ferrari <sebas5384@gmail.com>
2019-01-17 11:24:38 -0200

    step

--- diamante.test.js
+++ diamante.test.js
@@ -1,32 +1,36 @@
 const { diamond, getLetters } = require('./diamond.js')
 
-const diamonds = {
-  a: `a\na`,
-  a: `a\na`,
-}
+describe('Diamonds', () => {
+  describe('getLetters', () => {
+    it('should return letters to be printed when given E', () => {
+      expect(getLetters('e')).toEqual(['a', 'b', 'c', 'd', 'e'])
+    })
 
-describe('getLetters', () => {
-  it('should return letters to be printed when given E', () => {
-    expect(getLetters('e')).toEqual(['a', 'b', 'c', 'd', 'e'])
+    it('should return letters to be printed when given C', () => {
+      expect(getLetters('c')).toEqual(['a', 'b', 'c'])
+    })
+
+    it('should return letters to be printed when given D', () => {
+      expect(getLetters('d')).toEqual(['a', 'b', 'c', 'd'])
+    })
+
+    it('should return letters to be printed when given F', () => {
+      expect(getLetters('f')).toEqual(['a', 'b', 'c', 'd', 'e', 'f'])
+    })
   })
 
-  it('should return letters to be printed when given C', () => {
-    expect(getLetters('c')).toEqual(['a', 'b', 'c'])
-  })
+  describe.only('diamond', () => {
+    const diamonds = {
+      a: `a\na`,
+      a: `a\na`,
+    }
 
-  it('should return letters to be printed when given D', () => {
-    expect(getLetters('d')).toEqual(['a', 'b', 'c', 'd'])
-  })
+    it('should return diamon from A', () => {
+      expect(diamond('a')).toEqual('a\na')
+    })
 
-  it('should return letters to be printed when given F', () => {
-    expect(getLetters('f')).toEqual(['a', 'b', 'c', 'd', 'e', 'f'])
+    // it('should return diamon from B', () => {
+    //   expect(diamond('b')).toEqual(' a \nb b\n a ')
+    // })
   })
-
-  it('should return diamon from A', () => {
-    expect(diamond('a')).toEqual(a)
-  })
-
-  // it('should return diamon from B', () => {
-  //   expect(diamond('b')).toEqual(' a \nb b\n a ')
-  // })
 })
