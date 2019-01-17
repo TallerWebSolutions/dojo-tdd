5e52ac367aa35581e4ac38f5b52b694194cda3a2
Sebastian Ferrari <sebas5384@gmail.com>
2019-01-17 11:04:37 -0200

    step

---
+++ diamante.test.js
@@ -0,0 +1,27 @@
+const { diamond, getLetters } = require('./diamond.js')
+
+describe('Diamantes', () => {
+  it('should return letters to be printed when given E', () => {
+    expect(getLetters('e')).toEqual(['a', 'b', 'c', 'd', 'e'])
+  })
+
+  it('should return letters to be printed when given C', () => {
+    expect(getLetters('c')).toEqual(['a', 'b', 'c'])
+  })
+
+  it('should return letters to be printed when given D', () => {
+    expect(getLetters('d')).toEqual(['a', 'b', 'c', 'd'])
+  })
+
+  it('should return letters to be printed when given F', () => {
+    expect(getLetters('f')).toEqual(['a', 'b', 'c', 'd', 'e', 'f'])
+  })
+
+  it('should return diamon from A', () => {
+    expect(diamond(['a'])).toEqual('a\na')
+  })
+
+  it('should return diamon from B', () => {
+    expect(diamond(['a', 'b'])).toEqual(' a \nb b\n a ')
+  })
+})
---
+++ diamond.js
@@ -0,0 +1,58 @@
+// lucas testando aqui!
+
+const getLetters = letter => {
+  const initial = 'a'.charCodeAt()
+  const final = letter.charCodeAt()
+  const gap = final - initial
+  const diamondLetters = Array.from({ length: gap + 1 })
+    .fill()
+    .map((_, key) => initial + key)
+    .map(charCode => String.fromCharCode(charCode))
+
+  return diamondLetters
+}
+
+const diamond = arr => {
+  const length = arr.length
+  const initialSpaces = length
+
+  return arr
+    .map((letter, index) => {
+      if (index === 0) return `${letter}\n`
+      if (index === 1) return `${letter} ${letter}\n`
+      if (index === 2) return `${letter}   ${letter}\n`
+    })
+    .join('')
+}
+
+/**
+ * Espaços dos lados
+ * 1 - 0
+ * 2 - 1,0
+ * 3 - 2,1,0
+ */
+
+/**
+ * Espaços do meio
+ * 1 - 0
+ * 2 - 0,1
+ * 3 - 0,1,3
+ */
+
+/**
+ *  a
+ * b b
+ *  a
+ */
+
+/**
+ *   a
+ *  b b
+ * c   c
+ *  a
+ */
+
+module.exports = {
+  diamond,
+  getLetters,
+}
