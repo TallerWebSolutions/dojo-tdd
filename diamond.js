e98b835ddafad445942da6075cc69fc252006f6b
Sebastian Ferrari <sebas5384@gmail.com>
2019-01-17 12:04:40 -0200

    step

--- diamond.js
+++ diamond.js
@@ -21,7 +21,7 @@ const getDiamondLine = (letter, i, len) => {
     space = space.concat(' ')
   }
 
-  for (let j = len; j === 0; j--) {
+  for (let j = len; j <= len; j--) {
     spaceBefore = spaceBefore.concat(' ')
   }
 
