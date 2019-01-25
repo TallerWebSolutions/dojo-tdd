a106f3fe1deec36260bcecc584ded0fd615b2729
Sebastian Ferrari <sebas5384@gmail.com>
2019-01-25 11:13:50 -0200

    step

--- diamond.js
+++ diamond.js
@@ -3,23 +3,19 @@
 const getLetters = letter => {
   const initial = 'a'.charCodeAt()
   const final = letter.charCodeAt()
-  const gap = final - initial
-  const diamondLetters = Array.from(Array(gap + 1))
+
+  return Array.from(Array(final - initial + 1))
     .map((_, key) => initial + key)
     .map(charCode => String.fromCharCode(charCode))
-
-  return diamondLetters
 }
 
 const makeSpaces = n => new Array(n).fill(' ').join('')
-
 const makeSpacesBefore = (i, len) => makeSpaces(len - i - 1)
-
 const makeSpacesBetween = i => makeSpaces(Math.max(0, i * 2 - 1))
 
 const getDiamondLine = (letter, i, arr) => {
-  let spaceBefore = makeSpacesBefore(i, arr.length)
-  let spaceBetween = makeSpacesBetween(i)
+  const spaceBefore = makeSpacesBefore(i, arr.length)
+  const spaceBetween = makeSpacesBetween(i)
 
   if (letter == 'a') return `${spaceBefore}${letter}`
 
