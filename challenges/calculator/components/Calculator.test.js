9bfc20428fed3e95938cddf39da4588e4a58afc9
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-31 14:45:39 -0200

    added calculator

--- challenges/calculator/components/Calculator.test.js
+++ challenges/calculator/components/Calculator.test.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import { render } from 'enzyme'
+
 import Calculator from './Calculator'
 
 describe('Calculator', () => {
-  it('should render component', () => {})
+  it('should render component', () => {
+    const wrapper = render(<Calculator />)
+
+    expect(wrapper.text()).toBe('calculadora')
+  })
 })
