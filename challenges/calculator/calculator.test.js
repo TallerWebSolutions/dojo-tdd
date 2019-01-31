67c310a5c8d057ad767437bf1ee4854738be5673
Sebastian Ferrari <sebas5384@gmail.com>
2019-01-31 14:23:58 -0200

    step

--- challenges/calculator/calculator.test.js
+++ challenges/calculator/calculator.test.js
@@ -1,9 +1,13 @@
 const { sum } = require('./calculator')
 
 describe('calculator', () => {
-  describe('calc', () => {
-    it('should work', () => {
+  describe('sum', () => {
+    it('should sum two integers', () => {
       expect(sum(1, 1)).toBe(2)
+    })
+
+    it('should throw error when sum string', () => {
+      expect(sum(1, 'b')).toThrowError()
     })
   })
 })
