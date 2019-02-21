3cef8bf555806c84f5b074b61648e5f28f483d2c
Sebastian Ferrari <sebas5384@gmail.com>
2019-02-21 11:49:18 -0300

    step

--- challenges/calculator/components/Calculator.js
+++ challenges/calculator/components/Calculator.js
@@ -4,12 +4,16 @@ import UserInput from './UserInput'
 import Result from './Result'
 import Button from './Button'
 
-const Calculator = () => (
-  <div>
-    <UserInput />
-    <Result />
-    <Button />
-  </div>
-)
+class Calculator extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <UserInput />
+        <Result />
+        <Button />
+      </div>
+  )}
+}
 
 export default Calculator
