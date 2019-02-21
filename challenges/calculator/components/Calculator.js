5f33485c9d8ffc5be39c75677e19109634b3b540
Sebastian Ferrari <sebas5384@gmail.com>
2019-02-21 11:29:17 -0300

    step

--- challenges/calculator/components/Calculator.js
+++ challenges/calculator/components/Calculator.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import UserInput from './UserInput'
+import Result from './Result'
+import Button from './Button'
 
-const Calculator = () => <div>calculadora</div>
+const Calculator = () => (
+  <div>
+    <UserInput />
+    <Result />
+    <Button />
+  </div>
+)
 
 export default Calculator
