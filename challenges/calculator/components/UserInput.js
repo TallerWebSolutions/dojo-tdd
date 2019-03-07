830e616d793e018e61f74509778c969504891f32
Sebastian Ferrari <sebas5384@gmail.com>
2019-03-07 10:14:18 -0300

    step

--- challenges/calculator/components/UserInput.js
+++ challenges/calculator/components/UserInput.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const UserInput = ({ value }) => (
-  <input type='text' value={ value } />
+  <input type='text' value={ value } onChange={ onChange } />
 )
 
 export default UserInput
