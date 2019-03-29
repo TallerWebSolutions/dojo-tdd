a4e1293a1f298d0d83ad5555bce674020ea5bcb9
Sebastian Ferrari <sebas5384@gmail.com>
2019-03-29 10:25:14 -0300

    step

--- challenges/calculator/components/Calculator.test.js
+++ challenges/calculator/components/Calculator.test.js
@@ -78,6 +78,7 @@ describe('Calculator', () => {
     wrapper.find(ResetButton).simulate('click')
 
     expect(wrapper.state()).toEqual({ input: '', result: '' })
+    expect(ResetButton.value).toEqual('')
   })
 
   it('should have number buttons from 0 a 9', () => {
