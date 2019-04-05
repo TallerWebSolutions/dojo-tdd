31f39bfc4ba4763171e15cb8fc58c1739ef2b830
Sebastian Ferrari <sebas5384@gmail.com>
2019-04-05 12:46:34 -0300

    step

--- challenges/euler/euler.js
+++ challenges/euler/euler.js
@@ -1,7 +1,7 @@
 export const isMultiple = a => b => b % a === 0
 export const isMultipleThree = isMultiple(3)
 export const isMultipleFive = isMultiple(5)
-export const range = n => [...Array(n).keys()]
+export const range = n => new Array(n).fill().map((a, i) => i + 1)
 export const rangeBelow = n => range(n - 1)
 export const filterMultiples = a => a.filter(isMultipleByThreeOrFive)
 
@@ -9,4 +9,5 @@ export const isMultipleByThreeOrFive = n =>
   isMultipleThree(n) || isMultipleFive(n)
 
 export const sum = arr => arr.reduce((acc, cur) => acc + cur)
-export const resultExpectedEulerOne = num => sum(filterMultiples(range(num)))
+export const resultExpectedEulerOne = num =>
+  sum(filterMultiples(rangeBelow(num)))
