ef27ea0c8162d1fc7e3a13477efe109edb7b1537
Sebastian Ferrari <sebas5384@gmail.com>
2019-04-05 12:31:33 -0300

    step

--- challenges/euler/euler.test.js
+++ challenges/euler/euler.test.js
@@ -97,4 +97,12 @@ describe.only('Sum', () => {
   it('should return sum the elements of arr', () => {
     expect(sum([1, 2])).toEqual(3)
   })
+
+  it('should return sum the elements of arr', () => {
+    expect(sum([1, 2])).toEqual(3)
+  })
+
+  it('should return sum the elements of arr', () => {
+    expect(sum([1, 2])).toEqual(3)
+  })
 })
