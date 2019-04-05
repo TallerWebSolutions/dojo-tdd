cf89440aade5daa5d0dbb4a477021c51602c7cca
Sebastian Ferrari <sebas5384@gmail.com>
2019-04-05 12:41:33 -0300

    step

--- challenges/euler/euler.js
+++ challenges/euler/euler.js
@@ -1,7 +1,7 @@
 export const isMultiple = a => b => b % a === 0
 export const isMultipleThree = isMultiple(3)
 export const isMultipleFive = isMultiple(5)
-export const range = n => new Array(n).fill().map((a, i) => i + 1)
+export const range = n => [...Array(n).keys()]
 export const rangeBelow = n => range(n - 1)
 export const filterMultiples = a => a.filter(isMultipleByThreeOrFive)
 
