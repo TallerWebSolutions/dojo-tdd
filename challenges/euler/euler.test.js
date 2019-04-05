f2fcfacf5e37e5bc0a92bac45e768f2d9a2b38cf
Sebastian Ferrari <sebas5384@gmail.com>
2019-04-05 10:28:24 -0300

    step

--- challenges/euler/euler.test.js
+++ challenges/euler/euler.test.js
@@ -1,8 +1,12 @@
 const isMultipleThree = number => false
 
 describe('euler', () => {
-  it('should detect if is multiple of 3', () => {
+  it('should detect if 2 is multiple of 3', () => {
     const result = isMultipleThree(2)
     expect(result).toBeFalsy()
+  })
+  it('should detect if 3 is multiple of 3', () => {
+    const result = isMultipleThree(3)
+    expect(result).toBeTruthy()
   })
 })
