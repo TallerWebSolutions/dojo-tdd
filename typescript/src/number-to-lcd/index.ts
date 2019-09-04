8831b5aba0e8e4ef990229cfe00f0b5637ae9346
Arthur Almeida <arthur.almeidapereira@gmail.com>
2019-09-04 09:58:21 -0300

    number-to-lcd kata

--- typescript/src/number-to-lcd/index.ts
+++ typescript/src/number-to-lcd/index.ts
@@ -1,22 +1,41 @@
-export const digitToLCD = (digit: number): string => {
-  const digitsMap = {
-    "1": ["   ", "  |", "  |"],
-    "2": [" _ ", " _|", "|_ "],
-    "3": [" _ ", " _|", " _|"],
-    "4": ["   ", "|_|", "  |"],
-    "5": [" _ ", "|_ ", " _|"],
-    "6": [" _ ", "|_ ", "|_|"],
-    "7": [" _ ", "  |", "  |"],
-    "8": [" _ ", "|_|", "|_|"],
-    "9": [" _ ", "|_|", " _|"]
-  };
+const digitsMap = {
+  "1": ["   ", "  |", "  |"],
+  "2": [" _ ", " _|", "|_ "],
+  "3": [" _ ", " _|", " _|"],
+  "4": ["   ", "|_|", "  |"],
+  "5": [" _ ", "|_ ", " _|"],
+  "6": [" _ ", "|_ ", "|_|"],
+  "7": [" _ ", "  |", "  |"],
+  "8": [" _ ", "|_|", "|_|"],
+  "9": [" _ ", "|_|", " _|"]
+};
 
+export const digitToLCD = (digit: number, width: number = 1): string => {
   const digits = String(digit).split("");
   const result = ["", "", ""];
+  const changeWidthBy = changeWidth(width);
 
   return result
     .map((row, index) => {
-      return digits.map(digit => digitsMap[digit][index]).join("");
+      return digits
+        .map(digit => changeWidthBy(digitsMap[digit][index]))
+        .join("");
     })
     .join("\n");
+};
+
+export const changeWidth = (width: number) => (lcdNumber: string) => {
+  return lcdNumber[0] + lcdNumber[1].repeat(width) + lcdNumber[2];
+};
+
+export const changeHeight = (height: number) => (lcdNumber: string) => {
+  const foo = digitsMap[lcdNumber];
+
+  return [
+    foo[0],
+    foo[1].replace("_", " "),
+    foo[1],
+    foo[2].replace("_", " "),
+    foo[2]
+  ];
 };
--- typescript/src/number-to-lcd/number-to-lcd.test.ts
+++ typescript/src/number-to-lcd/number-to-lcd.test.ts
@@ -1,4 +1,4 @@
-import { digitToLCD } from "./";
+import { digitToLCD, changeWidth, changeHeight } from "./";
 
 describe("should render LCD format", () => {
   it("Should render number 1", () => {
@@ -23,8 +23,65 @@ describe("should render LCD format", () => {
     expect(digitToLCD(9)).toEqual(" _ \n|_|\n _|");
   });
 
-  it.only("Should render an arbitrary number", () => {
+  it("Should render an arbitrary number", () => {
     const mockDigits = "    _ \n  | _|\n  ||_ ";
     expect(digitToLCD(12)).toEqual(mockDigits);
+  });
+
+  describe("should support custom dimensions, width and height", () => {
+    it("should render the number 1 with width 3", () => {
+      const expected = "     \n    |\n    |";
+      expect(digitToLCD(1, 3)).toEqual(expected);
+    });
+
+    it("should render the number 2 with custom width", () => {
+      const expected = " __ \n __|\n|__ ";
+      expect(digitToLCD(2, 2)).toEqual(expected);
+    });
+  });
+  describe("should multiply line width by a factor of [parameter]", () => {
+    it("should multiply spaces by 2", () => {
+      expect(changeWidth(2)("   ")).toEqual("    ");
+    });
+
+    it("should multiply underscore by 2", () => {
+      expect(changeWidth(2)(" _ ")).toEqual(" __ ");
+    });
+
+    it("should multiply underscore by 2", () => {
+      expect(changeWidth(2)(" _ ")).toEqual(" __ ");
+    });
+
+    it("should multiply underscore by 3", () => {
+      const changeWidthBy3 = changeWidth(3);
+      expect(changeWidthBy3(" _ ")).toEqual(" ___ ");
+    });
+  });
+
+  describe.only("should increase line height", () => {
+    it("should increase the height of number 1", () => {
+      const expected = ["   ", "  |", "  |", "  |", "  |"];
+      const changeHeightBy2 = changeHeight(2);
+      expect(changeHeightBy2("1")).toEqual(expected);
+    });
+
+    it("should increase the height of number 2", () => {
+      const expected = [" _ ", "  |", " _|", "|  ", "|_ "];
+      const changeHeightBy2 = changeHeight(2);
+
+      expect(changeHeightBy2("2")).toEqual(expected);
+    });
+
+    it("should increase the height of number 3", () => {
+      const expected = ["   ", "  |", "  |", "  |", "  |", "  |", "  |"];
+      const changeHeightBy3 = changeHeight(3);
+      expect(changeHeightBy3("1")).toEqual(expected);
+    });
+
+    it("should increase the height of number 4", () => {
+      const expected = ["   ", "  |", "  |", "  |", "  |", "  |", "  |"];
+      const changeHeightBy4 = changeHeight(4);
+      expect(changeHeightBy4("4")).toEqual(expected);
+    });
   });
 });
