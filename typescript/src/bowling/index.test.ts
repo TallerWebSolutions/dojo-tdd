78869283c5ddcebc6d7f9c542e18185d3a822ba6
Evelyn Schmitz <evelynschmitz1@gmail.com>
2019-12-04 12:25:21 -0300

    refactor: updated calculateResult and added getFirstTurn, getLastTurn, getNextFrame.

--- typescript/src/bowling/index.test.ts
+++ typescript/src/bowling/index.test.ts
@@ -1,11 +1,14 @@
 import {
-  calculateFrame,
-  calculateResult,
-  isStrike,
-  Frame,
-  Game,
-  isSpare
-} from "./"
+	calculateFrame,
+	calculateResult,
+	isStrike,
+	Frame,
+	Game,
+	isSpare,
+	getFirstTurn,
+	getLastTurn,
+	getNextFrame
+} from './';
 
 describe("Calculate the points in a frame", () => {
   it("Calculate the points sum in only one frame", () => {
@@ -114,6 +117,23 @@ describe("Calculate the results in a game", () => {
       expect(calculateResult(game)).toEqual(140)
     })
   })
+
+  it('should calculate perfect game', () => {
+    const game: Game = [
+			[10, 0], //30
+			[10, 0], //30
+			[10, 0], //30
+			[10, 0], //30
+			[10, 0], //30
+			[10, 0], //30
+			[10, 0], //30
+			[10, 0], //30
+			[10, 0], //30
+			[10, 10, 10] // 30
+		];
+
+    expect(calculateResult(game)).toEqual(300)
+  })
 })
 
 describe("is strike", () => {
@@ -130,5 +150,43 @@ describe("is spare", () => {
     expect(isSpare([5, 5])).toEqual(true)
     expect(isSpare([10, 0])).toEqual(false)
     expect(isSpare([0, 10])).toEqual(true)
+  })
+})
+
+describe("Frames", () => {
+  it("should return the first turn", () => {
+    const frame: Frame = [0,1] 
+    expect(getFirstTurn(frame)).toEqual(0)
+    
+    const frame2: Frame = [2,1] 
+    expect(getFirstTurn(frame2)).toEqual(2)
+    
+    const frame3: Frame = [3,1] 
+    expect(getFirstTurn(frame3)).not.toEqual(1)
+  })
+
+  it("should return the last turn", () => {
+    const frame: Frame = [0,1] 
+    expect(getLastTurn(frame)).toEqual(1)
+    
+    const frame2: Frame = [0,3] 
+    expect(getLastTurn(frame2)).toEqual(3)
+    
+    const frame3: Frame = [0,4] 
+    expect(getLastTurn(frame3)).not.toEqual(0)
+  })
+
+  describe("getNextFrame", () => {
+    it("should return the next frame", () => {
+      const game: Game = [[0, 1], [3, 2], [4, 4]]
+      const index = 1
+      expect(getNextFrame(game, index)).toEqual([4, 4])
+    })
+
+    it("should return undefined at last frame", () => {
+      const game: Game = [[0, 1], [3, 2], [4, 4]]
+      const index = 2
+      expect(getNextFrame(game, index)).toEqual([0, 0])
+    })
   })
 })
--- typescript/src/bowling/index.ts
+++ typescript/src/bowling/index.ts
@@ -11,17 +11,30 @@ export const isStrike = (frame: Frame) => frame[0] === TOTAL_PINS
 export const calculateResult = (data: Frame[]) =>
   data.reduce((acc, frame, index) => {
     let extra: number = 0
-    const isLastFrame = index === 9
+    const isLastFrame = index === data.length - 1
 
     const wasSpare = isSpare(frame)
     const wasStrike = isStrike(frame)
 
+    const nextFrame = getNextFrame(data, index)
+
     if (wasSpare && !isLastFrame) {
-      extra = calculateFrame(data[index + 1])
+      extra = getFirstTurn(nextFrame);
     }
 
+    // Refactor?
     if (wasStrike && !isLastFrame) {
-      extra = calculateFrame(data[index + 1]) + calculateFrame(data[index + 2])
+      let secondFrame = 0;
+      if (data[index + 2]) {
+        if(isStrike(data[index + 2])) {
+          secondFrame = data[index + 2][0]
+        }
+        else {
+          secondFrame = calculateFrame([data[index + 2][0], data[index + 2][1]]);
+        }
+      }
+
+      extra = calculateFrame([getFirstTurn(nextFrame), getLastTurn(nextFrame)]) + secondFrame
     }
 
     return acc + calculateFrame(frame) + extra
@@ -29,3 +42,10 @@ export const calculateResult = (data: Frame[]) =>
 
 export const isSpare = (frame: Frame) =>
   !isStrike(frame) && calculateFrame(frame) === TOTAL_PINS
+
+
+  export const getFirstTurn = (frame: Frame) => frame[0]
+  
+  export const getLastTurn = (frame: Frame) => frame[1]
+
+  export const getNextFrame = (game: Game, index: number): Frame => game[index + 1] || [0,0]
